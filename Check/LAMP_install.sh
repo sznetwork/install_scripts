a579b6ae901be68a7334b049657407b07e3cd2d8
user13 <sznetworksystems@gmail.com>
2025-05-08 12:26:07 +0300

    - changed LAMP server script code

--- Check/LAMP_install.sh
+++ Check/LAMP_install.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-echo $(Script not tested.)
 # Update package index
 apt update
 # Install Apache
@@ -7,20 +6,28 @@ apt install -y apache2
 # Enable and start Apache service
 systemctl enable apache2
 systemctl start apache2
-# Install MySQL
-apt install -y mysql-server
-apt install -y mysql-common
+# Install MySQL (MariaDB)
+apt install -y mariadb-server mariadb-client
 # Secure MySQL installation
-mysql_secure_installation
+mysql_secure_installation <<EOF
+
+y
+password
+password
+y
+y
+y
+y
+EOF
 # Install PHP and necessary modules
 apt install -y php libapache2-mod-php php-mysql
 # Restart Apache to apply changes
 if command -v systemctl &> /dev/null; then
-    echo $(systemctl is available.)
+    echo "systemctl is available."
     systemctl restart apache2
 else
-    echo $(systemctl is not available.)
+    echo "systemctl is not available."
     service apache2 restart
 fi
 # Test PHP Processing
-echo $(<?php phpinfo(); ?>) | sudo tee /var/www/html/phpinfo.php
+echo '<?php phpinfo(); ?>' | sudo tee /var/www/html/phpinfo.php
